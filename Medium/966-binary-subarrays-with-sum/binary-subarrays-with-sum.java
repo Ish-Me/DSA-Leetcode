36e3efdbd9061fd724a110967da19d9c03d173ca
Ishaan Mehrotra <ishaanmehrotra2@gmail.com>
2026-06-30 22:47:22 +0530

    Time: 1 ms (100.00%) | Memory: 50.8 MB (75.41%) - LeetSync

---
+++ Medium/966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int SubarraysCount(int[] nums,int goal){
+         if(goal<0)return 0;
+        int l=0,r=0,sum=0,cnt=0;
+        while(r<nums.length){
+            sum+=nums[r];
+            while(sum>goal){
+                sum=sum-nums[l];
+                l++;
+            }
+            cnt=cnt+(r-l+1);
+            r++;
+        }
+        return cnt;
+    }
+    public int numSubarraysWithSum(int[] nums, int goal) {
+        return SubarraysCount(nums,goal)-SubarraysCount(nums,goal-1);
+    }
+}
