ff640d790a5815fa33c6b24ceb5c73e575072a7c
Ishaan Mehrotra <ishaanmehrotra2@gmail.com>
2026-07-20 20:39:03 +0530

    Time: 1 ms (87.24%) | Memory: 47.4 MB (31.60%) - LeetSync

--- Medium/547-number-of-provinces/number-of-provinces.java
+++ Medium/547-number-of-provinces/number-of-provinces.java
@@ -1,37 +1,26 @@
 class Solution {
     public int findCircleNum(int[][] isConnected) {
-        List<List<Integer>> adjList = new ArrayList<>();
         int V = isConnected.length;
-        for (int i = 0; i < V; i++) {
-            adjList.add(new ArrayList<>());
-        }
-        for (int i = 0; i < V; i++) {
-            for (int j = 0; j < V; j++) {
-                if (isConnected[i][j] == 1 && i != j) {
-                    adjList.get(i).add(j);
-                }
-            }
-        }
         int count = 0;
         int[] vis = new int[V];
         for (int i = 0; i < V; i++) {
             if (vis[i] == 0) {
                 count++;
-                bfs(i, vis, adjList);
+                bfs(i, vis, isConnected);
             }
         }
         return count;
     }
-    public static void bfs(int node, int[] vis, List<List<Integer>> adjList) {
+    public static void bfs(int node, int[] vis, int[][] isConnected) {
         Queue<Integer> queue = new LinkedList<>();
         queue.add(node);
         vis[node] = 1;
         while (!queue.isEmpty()) {
             Integer startingNode = queue.poll();
-            for (Integer neighbour : adjList.get(startingNode)) {
-                if (vis[neighbour] != 1) {
-                    vis[neighbour] = 1;
-                    queue.add(neighbour);
+            for (int i=0;i<isConnected.length;i++) {
+                if (isConnected[startingNode][i]==1 && vis[i]==0) {
+                    vis[i] = 1;
+                    queue.add(i);
                 }
             }
         }
