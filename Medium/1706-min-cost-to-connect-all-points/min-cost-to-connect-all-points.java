3ea8ea659fd37cc5bc4b90b0e9abe2a293d5423a
Ishaan Mehrotra <ishaanmehrotra2@gmail.com>
2026-08-03 19:33:26 +0530

    Time: 1329 ms (5.01%) | Memory: 129.2 MB (19.82%) - LeetSync

---
+++ Medium/1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int minCostConnectPoints(int[][] points) {
+
+        int n = points.length;
+        boolean[] visited = new boolean[n];
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[0] - b[0]);
+        pq.offer(new int[]{0, 0});
+        int totalCost = 0;
+
+        while (pq.size()!=0) {
+            int[] curr = pq.poll();
+
+            int cost = curr[0];
+            int node = curr[1];
+
+            if (visited[node])
+                continue;
+
+            visited[node] = true;
+            totalCost += cost;
+
+            for (int next = 0; next < n; next++) {
+                    int distance = Math.abs(points[node][0] - points[next][0]) +
+                                   Math.abs(points[node][1] - points[next][1]);
+
+                    pq.offer(new int[]{distance, next});
+                
+            }
+        }
+
+        return totalCost;
+    }
+}
